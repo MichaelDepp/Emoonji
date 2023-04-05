c36f4512da39fd41ee0b28160aa5b86982230e79
MichaelDepp <michaeldepp7@gmail.com>
2023-04-05 22:39:57 +0800

    Revert "refactor : upgrade to node 18"
    This reverts commit 5305e046e759013a7f8ba40e0e4cb94bd1c39121.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,27 @@
-import { Box, Flex, Heading, Button, useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  useColorMode,
+  useColorModeValue,
+  IconButton,
+} from "@chakra-ui/core";
 
 function Navbar() {
-	const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
 
-	const bg = useColorModeValue('#F2F2F2', '#1A1A18');
-	const color = useColorModeValue('#1A1A18', '#F2F2F2');
-	return (
-		<Flex bg={bg} w="100%" justifyContent="center" alignItems="center">
-			<Flex flexDirection="row" justifyContent="center" alignItems="center">
-				<Heading pl={3} color={color} fontWeight={700}>
-					Emoonji
-				</Heading>
-			</Flex>
-		</Flex>
-	);
+  const bg = useColorModeValue("#F2F2F2", "#1A1A18");
+  const color = useColorModeValue("#1A1A18", "#F2F2F2");
+  return (
+    <Flex bg={bg} w="100%" justifyContent="center" alignItems="center">
+      <Flex flexDirection="row" justifyContent="center" alignItems="center">
+        <Heading pl={3} color={color} fontWeight={700}>
+          Emoonji
+        </Heading>
+      </Flex>
+    </Flex>
+  );
 }
 
 export default Navbar;
--- next.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-	webpack: (config) => {
-		if (config.target === 'web') config.externals = ['fs', 'os'];
-		return config;
-	},
-};
-
-module.exports = config;
